081ec9fc2103cfe95097b54ae5c55349c0f9d84b
jess <jess1104jess1104@gmail.com>
2021-11-08 18:11:01 +0800

    init & form valid attempt

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ui-textbox
+      error="The username may not be more than 16 characters"
+      help="Pick a unique username not more than 16 characters"
+      label="Username"
+      placeholder="Enter a username"
+      :maxlength="16"
+      :invalid="textbox11.length > 16"
+      v-model="textbox11"
+    ></ui-textbox>
+    <ui-textbox
+      autocomplete="off"
+      error="This field is required"
+      help="If you have multiple names, enter the one you prefer"
+      label="Name"
+      placeholder="Enter your name"
+      required
+      :invalid="textbox10Touched && textbox10.length === 0"
+      @touch="textbox10Touched = true"
+      v-model="textbox10"
+    ></ui-textbox>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      textbox11: "",
+      textbox10: "",
+      textbox10Touched: false,
+    };
+  },
+};
 </script>
 
 <style>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
+import KeenUI from "keen-ui";
+import "keen-ui/dist/keen-ui.css";
 
-Vue.config.productionTip = false
+Vue.use(KeenUI);
+
+new Vue({
+  components: {
+    // all Keen UI components already registered
+  },
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
