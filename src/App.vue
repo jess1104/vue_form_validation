6e5147d9fbb5001427c4f8c9ccbc1c0aed007a5a
unknown <lalab996@hotmail.com.tw>
2021-11-14 23:58:38 +0800

    feat: fix

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,51 @@
 <template>
   <div id="app" class="app">
     <form>
-      <!-- 帳號 -->
-      <ui-textbox
-        label="帳號"
-        icon="person"
-        placeholder="請輸入帳號"
-        help="需輸入4-20碼英文小寫及數字"
-        error="帳號必填，需介於4-20碼之間的英文與數字"
-        :invalid="account.isValid === false"
-        :maxlength="20"
-        @input="validAccount"
-        :value="account.text"
-        :disabled="submitVal"
-      ></ui-textbox>
-      <!-- 密碼 -->
-      <ui-textbox
-        label="密碼"
-        icon="lock"
-        placeholder="請輸入密碼"
-        help="需輸入6-20碼英文小寫及數字"
-        error="密碼必填，需介於6-20碼之間的英文與數字"
-        :invalid="password.isValid === false"
-        :maxlength="20"
-        @input="validPassword"
-        :value="password.text"
-        :disabled="submitVal"
-      ></ui-textbox>
-      <!-- 確認密碼 -->
-      <ui-textbox
-        label="確認密碼"
-        icon="lock"
-        placeholder="請確認密碼"
-        help="需與密碼相同"
-        error="必填，需與密碼相同"
-        :invalid="confirmPass.isValid === false"
-        :maxlength="20"
-        @input="validConfirmPass"
-        :value="confirmPass.text"
-        :disabled="submitVal"
-      ></ui-textbox>
-      <!-- 姓名 -->
-      <ui-textbox
-        label="姓名"
-        icon="face"
-        placeholder="請輸入姓名"
-        help="需輸入姓名，不可輸入數字、空白及特殊符號"
-        error="必填，不可輸入數字、空白及特殊符號"
-        :invalid="name.isValid === false"
-        @input="validName"
-        :value="name.text"
-        :disabled="submitVal"
-      ></ui-textbox>
-      <ui-select
-        label="性別"
-        icon="transgender"
-        placeholder="請選擇性別"
-        :options="genderOption"
-        @change="changeGender"
-        :value="gender"
-        :disabled="submitVal"
-      ></ui-select>
-      <ui-textbox
-        label="信箱"
-        icon="mail"
-        placeholder="請輸入信箱"
-        help="請輸入電子信箱"
-        error="選填，不可輸入數字、空白及特殊符號"
-        :invalid="email.isValid === false"
-        @input="validMail"
-        :value="email.text"
-        :disabled="submitVal"
-      ></ui-textbox>
-      <ui-button color="green" @click.prevent="submit(allValid)"
-        >送出</ui-button
-      >
+      <template v-for="form in forms">
+        <!-- textbox -->
+        <ui-textbox
+          v-if="form.type == 'textbox'"
+          :key="form.alias"
+          :label="form.label"
+          :icon="form.icon"
+          :placeholder="form.placeholder"
+          :help="form.help"
+          :error="form.error"
+          :invalid="!form.isValid"
+          @input="validForm(form.alias, $event)"
+          :value="form.value"
+          :disabled="form.disabled"
+        ></ui-textbox>
+
+        <!-- select -->
+        <ui-select
+          v-else-if="form.type == 'select'"
+          :key="form.alias"
+          :label="form.label"
+          :icon="form.icon"
+          :placeholder="form.placeholder"
+          :options="form.option"
+          @input="validSelect(form.alias, $event)"
+          :value="form.value"
+          :disabled="form.disabled"
+        ></ui-select>
+      </template>
+      <ui-button color="green" @click.prevent="submit(forms)">送出</ui-button>
     </form>
     <ul class="content" v-if="submitVal">
-      <li>帳號:{{ account.text }}</li>
+      <template v-for="form in forms">
+        <li v-if="form.type == 'textbox'" :key="form.alias">
+          {{ form.label }} : {{ form.value }}
+        </li>
+        <li v-else-if="form.type == 'select'" :key="form.alias">
+          {{ form.label }} : {{ form.value.label }}
+        </li>
+      </template>
+      <!-- <li>帳號:{{ account.text }}</li>
       <li>密碼:{{ password.text }}</li>
       <li>姓名:{{ name.text }}</li>
       <li v-if="gender">性別:{{ gender }}</li>
-      <li v-if="email.text">信箱:{{ email.text }}</li>
+      <li v-if="email.text">信箱:{{ email.text }}</li> -->
     </ul>
   </div>
 </template>
@@ -97,113 +61,165 @@ export default {
   },
   computed: {
     ...mapState([
-      "account",
-      "password",
-      "confirmPass",
-      "name",
-      "gender",
-      "genderOption",
-      "email",
+      "forms",
+      // "account",
+      // "password",
+      // "confirmPass",
+      // "name",
+      // "gender",
+      // "genderOption",
+      // "email",
     ]),
     ...mapGetters(["allValid"]),
   },
   methods: {
     ...mapActions([
-      "setValidAccount",
-      "setAccountText",
-      "setValidPassword",
-      "setPasswordText",
-      "setValidConfirmPass",
-      "setConfirmText",
-      "setValidName",
-      "setNameText",
-      "setGender",
-      "setValidMail",
-      "setMailText",
+      "setForm",
+      // "setValidAccount",
+      // "setAccountText",
+      // "setValidPassword",
+      // "setPasswordText",
+      // "setValidConfirmPass",
+      // "setConfirmText",
+      // "setValidName",
+      // "setNameText",
+      // "setGender",
+      // "setValidMail",
+      // "setMailText",
     ]),
-    validAccount(value) {
-      this.setAccountText(value);
+    validForm(alias, value) {
+      const aliasData = this.forms.find(item => item.alias == alias);
 
-      // 判斷長度及有無輸入
-      if (value.length > 20 || value.length < 4) {
-        this.setValidAccount(false);
-        return;
-      }
-      // 判斷正則英文與數字
-      if (/^[a-z0-9]+$/.test(value)) {
-        this.setValidAccount(true);
-      } else {
-        this.setValidAccount(false);
-      }
-    },
-    validPassword(value) {
-      this.setPasswordText(value);
+      this.setForm({
+        'alias': alias,
+        'valid': true,
+        'value': value
+      });
 
-      // 判斷長度及有無輸入
-      if (value.length > 20 || value.length < 6) {
-        this.setValidPassword(false);
-        return;
-      }
-      // 判斷正則英文與數字
-      if (/^[a-z0-9]+$/.test(value)) {
-        this.setValidPassword(true);
-      } else {
-        this.setValidPassword(false);
-      }
-      // 判斷有重新修改密碼後
-      if (value !== this.confirmPass.text) {
-        this.setValidConfirmPass(false);
-      }
+      //先驗證
+      aliasData.tests.forEach(element => {
+        if (element.testType == 'length') {//長度驗證
+          if (value.length > element.props.maxlen || value.length < element.props.minlen) {
+            this.setForm({
+              'alias': alias,
+              'valid': false,
+              'error': element.error,
+              'value': value
+            });
+          }
+        } else if (element.testType == 'reg') { //正則驗證
+          const doTest = new RegExp(element.test, 'i').test(value);
+          if (!doTest) {
+            this.setForm({
+              'alias': alias,
+              'valid': false,
+              'error': element.error,
+              'value': value
+            });
+          }
+        }
+      });
+
     },
-    validConfirmPass(value) {
-      this.setConfirmText(value);
-      // 確認有無輸入
-      if (value.length === 0) {
-        this.setValidConfirmPass(false);
-        return;
-      }
-      // 與密碼相符發action
-      if (value === this.password.text) {
-        this.setValidConfirmPass(true);
-      } else {
-        this.setValidConfirmPass(false);
-      }
+    validSelect(alias, value) {
+      this.setForm({
+        'alias': alias,
+        'value': value
+      });
     },
-    validName(value) {
-      this.setNameText(value);
-      // 確認有無輸入
-      if (value.length === 0) {
-        this.setValidName(false);
-        return;
-      }
-      if (/^[a-zA-Z\u4E00-\u9FA5]+$/.test(value)) {
-        this.setValidName(true);
-      } else {
-        this.setValidName(false);
-      }
-    },
-    changeGender(genderRes) {
-      this.setGender(genderRes);
-    },
-    validMail(value) {
-      this.setMailText(value);
-      // 不輸入也可
-      if (value.length === 0) {
-        this.setValidMail(true);
-        return;
-      }
-      // 驗證mail
-      let emailRule =
-        /^\w+((-\w+)|(\.\w+))*@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/;
-      if (emailRule.test(value)) {
-        this.setValidMail(true);
-      } else {
-        this.setValidMail(false);
-      }
-    },
+    // validAccount(value) {
+    //   this.setAccountText(value);
+
+    //   // 判斷長度及有無輸入
+    //   if (value.length > 20 || value.length < 4) {
+    //     this.setValidAccount(false);
+    //     return;
+    //   }
+    //   // 判斷正則英文與數字
+    //   if (/^[a-z0-9]+$/.test(value)) {
+    //     this.setValidAccount(true);
+    //   } else {
+    //     this.setValidAccount(false);
+    //   }
+    // },
+    // validPassword(value) {
+    //   this.setPasswordText(value);
+
+    //   // 判斷長度及有無輸入
+    //   if (value.length > 20 || value.length < 6) {
+    //     this.setValidPassword(false);
+    //     return;
+    //   }
+    //   // 判斷正則英文與數字
+    //   if (/^[a-z0-9]+$/.test(value)) {
+    //     this.setValidPassword(true);
+    //   } else {
+    //     this.setValidPassword(false);
+    //   }
+    //   // 判斷有重新修改密碼後
+    //   if (value !== this.confirmPass.text) {
+    //     this.setValidConfirmPass(false);
+    //   }
+    // },
+    // validConfirmPass(value) {
+    //   this.setConfirmText(value);
+    //   // 確認有無輸入
+    //   if (value.length === 0) {
+    //     this.setValidConfirmPass(false);
+    //     return;
+    //   }
+    //   // 與密碼相符發action
+    //   if (value === this.password.text) {
+    //     this.setValidConfirmPass(true);
+    //   } else {
+    //     this.setValidConfirmPass(false);
+    //   }
+    // },
+    // validName(value) {
+    //   this.setNameText(value);
+    //   // 確認有無輸入
+    //   if (value.length === 0) {
+    //     this.setValidName(false);
+    //     return;
+    //   }
+    //   if (/^[a-zA-Z\u4E00-\u9FA5]+$/.test(value)) {
+    //     this.setValidName(true);
+    //   } else {
+    //     this.setValidName(false);
+    //   }
+    // },
+    // changeGender(genderRes) {
+    //   this.setGender(genderRes);
+    // },
+    // validMail(value) {
+    //   this.setMailText(value);
+    //   // 不輸入也可
+    //   if (value.length === 0) {
+    //     this.setValidMail(true);
+    //     return;
+    //   }
+    //   // 驗證mail
+    //   let emailRule =
+    //     /^\w+((-\w+)|(\.\w+))*@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/;
+    //   if (emailRule.test(value)) {
+    //     this.setValidMail(true);
+    //   } else {
+    //     this.setValidMail(false);
+    //   }
+    // },
     submit(value) {
-      // console.log("allValid:" + value);
-      this.submitVal = value;
+      this.forms.forEach(item => {
+        this.validForm(item.alias, item.value);
+      })
+
+      const inValids = this.forms.filter(item => item.isValid != true); // array 
+      if (inValids.length >= 0) { //有一個是沒驗證通過
+        //沒驗證通過
+      } else {
+        // console.log("allValid:" + value);
+        this.submitVal = value;
+      }
+
     },
   },
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,116 +1,205 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from "vue"
+import Vuex from "vuex"
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    account: {
-      text: "",
-      isValid: null,
-    },
-    password: {
-      text: "",
-      isValid: null,
-    },
-    confirmPass: {
-      text: "",
-      isValid: null,
-    },
-    name: {
-      text: "",
-      isValid: null,
-    },
-    email: {
-      text: "",
-      isValid: true,
-    },
-    gender: "",
-    genderOption: ["男", "女", "中"],
-  },
-  getters: {
-    allValid(state) {
-      let arr = [
-        state.account.isValid,
-        state.password.isValid,
-        state.confirmPass.isValid,
-        state.name.isValid,
-        state.email.isValid,
-      ];
+    state: {
+        forms: [{
+                type: "textbox",
+                alias: "account",
+                label: "帳號",
+                icon: "person",
+                value: "",
+                placeholder: "請輸入帳號",
+                help: "需輸入4-20碼英文小寫及數字",
+                isValid: null,
+                disabled: false,
+                error: "",
+                tests: [{
+                        testType: "length",
+                        props: {
+                            minlen: 4,
+                            maxlen: 20,
+                        },
+                        error: "請輸入4-20個字元",
+                    },
+                    {
+                        testType: "reg",
+                        test: /^[a-z0-9]+$/,
+                        error: "請輸入英文小寫及數字",
+                    },
+                ],
+            },
+            {
+                type: "textbox",
+                alias: "address",
+                label: "住址",
+                icon: "person",
+                value: "",
+                placeholder: "請輸入住址",
+                help: "需輸入4-20碼英文小寫及數字",
+                isValid: null,
+                disabled: false,
+                error: "",
+                tests: [{
+                        testType: "length",
+                        props: {
+                            minlen: 4,
+                            maxlen: 20,
+                        },
+                        error: "請輸入4-20個字元",
+                    },
+                    {
+                        testType: "confirmPas",
+                        error: "請輸入英文小寫及數字",
+                    },
+                ],
+            },
+            {
+                type: "select",
+                alias: "gender",
+                label: "性別",
+                icon: "transgender",
+                value: "",
+                isValid: null,
+                disabled: false,
+                error: "",
+                option: [{
+                        label: "-",
+                        value: "",
+                    },
+                    {
+                        label: "男",
+                        value: 0,
+                    },
+                    {
+                        label: "女",
+                        value: 1,
+                    },
+                ],
+                tests: [],
+            },
+        ],
 
-      return arr.every((validValue) => {
-        return validValue === true;
-      });
+        account: {
+            text: "",
+            isValid: null,
+        },
+        password: {
+            text: "",
+            isValid: null,
+        },
+        confirmPass: {
+            text: "",
+            isValid: null,
+        },
+        name: {
+            text: "",
+            isValid: null,
+        },
+        email: {
+            text: "",
+            isValid: true,
+        },
+        gender: "",
+        genderOption: ["男", "女", "中"],
     },
-  },
-  mutations: {
-    SET_VALID_ACCOUNT(state, boolean) {
-      state.account.isValid = boolean;
+    getters: {
+        allValid(state) {
+            let arr = [
+                state.account.isValid,
+                state.password.isValid,
+                state.confirmPass.isValid,
+                state.name.isValid,
+                state.email.isValid,
+            ]
+
+            return arr.every((validValue) => {
+                return validValue === true
+            })
+        },
     },
-    SET_ACCOUNT_TEXT(state, newAccount) {
-      state.account.text = newAccount;
+    mutations: {
+        SET_FORM(state, payload) {
+            const formIndex = state.forms.findIndex(
+                (item) => item.alias == payload.alias
+            )
+            state.forms[formIndex]["isValid"] = payload.valid
+            state.forms[formIndex]["value"] = payload.value
+            state.forms[formIndex]["error"] = !payload.valid ? payload.error : ""
+        },
+        // SET_VALID_ACCOUNT(state, boolean) {
+        //     state.account.isValid = boolean
+        // },
+        // SET_ACCOUNT_TEXT(state, newAccount) {
+        //     state.account.text = newAccount
+        // },
+        // SET_VALID_PASSWPRD(state, boolean) {
+        //     state.password.isValid = boolean
+        // },
+        // SET_PASSWORD_TEXT(state, newPassword) {
+        //     state.password.text = newPassword
+        // },
+        // SET_VALID_CONFIRM_PASS(state, boolean) {
+        //     state.confirmPass.isValid = boolean
+        // },
+        // SET_CONFIRM_TEXT(state, newConfirm) {
+        //     state.confirmPass.text = newConfirm
+        // },
+        // SET_VALID_NAME(state, boolean) {
+        //     state.name.isValid = boolean
+        // },
+        // SET_NAME_TEXT(state, newName) {
+        //     state.name.text = newName
+        // },
+        // SET_GENDER(state, genderRes) {
+        //     state.gender = genderRes
+        // },
+        // SET_VALID_MAIL(state, boolean) {
+        //     state.email.isValid = boolean
+        // },
+        // SET_MAIL_TEXT(state, newMail) {
+        //     state.email.text = newMail
+        // },
     },
-    SET_VALID_PASSWPRD(state, boolean) {
-      state.password.isValid = boolean;
+    actions: {
+        setForm({ commit }, payload) {
+            commit("SET_FORM", payload)
+        },
+        // set({ commit, state }, payload) {},
+        // setValidAccount({ commit }, boolean) {
+        //   commit("SET_VALID_ACCOUNT", boolean);
+        // },
+        // setAccountText({ commit }, newAccount) {
+        //   commit("SET_ACCOUNT_TEXT", newAccount);
+        // },
+        // setValidPassword({ commit }, boolean) {
+        //   commit("SET_VALID_PASSWPRD", boolean);
+        // },
+        // setPasswordText({ commit }, newPassword) {
+        //   commit("SET_PASSWORD_TEXT", newPassword);
+        // },
+        // setValidConfirmPass({ commit }, boolean) {
+        //   commit("SET_VALID_CONFIRM_PASS", boolean);
+        // },
+        // setConfirmText({ commit }, newConfirm) {
+        //   commit("SET_CONFIRM_TEXT", newConfirm);
+        // },
+        // setValidName({ commit }, boolean) {
+        //   commit("SET_VALID_NAME", boolean);
+        // },
+        // setNameText({ commit }, newName) {
+        //   commit("SET_NAME_TEXT", newName);
+        // },
+        // setGender({ commit }, genderRes) {
+        //   commit("SET_GENDER", genderRes);
+        // },
+        // setValidMail({ commit }, boolean) {
+        //   commit("SET_VALID_MAIL", boolean);
+        // },
+        // setMailText({ commit }, newMail) {
+        //   commit("SET_MAIL_TEXT", newMail);
+        // },
     },
-    SET_PASSWORD_TEXT(state, newPassword) {
-      state.password.text = newPassword;
-    },
-    SET_VALID_CONFIRM_PASS(state, boolean) {
-      state.confirmPass.isValid = boolean;
-    },
-    SET_CONFIRM_TEXT(state, newConfirm) {
-      state.confirmPass.text = newConfirm;
-    },
-    SET_VALID_NAME(state, boolean) {
-      state.name.isValid = boolean;
-    },
-    SET_NAME_TEXT(state, newName) {
-      state.name.text = newName;
-    },
-    SET_GENDER(state, genderRes) {
-      state.gender = genderRes;
-    },
-    SET_VALID_MAIL(state, boolean) {
-      state.email.isValid = boolean;
-    },
-    SET_MAIL_TEXT(state, newMail) {
-      state.email.text = newMail;
-    },
-  },
-  actions: {
-    setValidAccount({ commit }, boolean) {
-      commit("SET_VALID_ACCOUNT", boolean);
-    },
-    setAccountText({ commit }, newAccount) {
-      commit("SET_ACCOUNT_TEXT", newAccount);
-    },
-    setValidPassword({ commit }, boolean) {
-      commit("SET_VALID_PASSWPRD", boolean);
-    },
-    setPasswordText({ commit }, newPassword) {
-      commit("SET_PASSWORD_TEXT", newPassword);
-    },
-    setValidConfirmPass({ commit }, boolean) {
-      commit("SET_VALID_CONFIRM_PASS", boolean);
-    },
-    setConfirmText({ commit }, newConfirm) {
-      commit("SET_CONFIRM_TEXT", newConfirm);
-    },
-    setValidName({ commit }, boolean) {
-      commit("SET_VALID_NAME", boolean);
-    },
-    setNameText({ commit }, newName) {
-      commit("SET_NAME_TEXT", newName);
-    },
-    setGender({ commit }, genderRes) {
-      commit("SET_GENDER", genderRes);
-    },
-    setValidMail({ commit }, boolean) {
-      commit("SET_VALID_MAIL", boolean);
-    },
-    setMailText({ commit }, newMail) {
-      commit("SET_MAIL_TEXT", newMail);
-    },
-  },
-});
+})
